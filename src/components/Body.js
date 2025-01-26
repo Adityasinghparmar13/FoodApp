3b395017c46e3993dc9c886851e61d23e954b0ce
Aditya Singh <138242529+Adityasinghparmar13@users.noreply.github.com>
2025-01-26 22:44:36 +0530

    API integration and Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,27 @@
 import RestCard from "./RestCard";
-import ResLis from "../utils/mockdata";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useState, useEffect } from "react";
 
 const Body = () => {
-    const [ListRec,setListRec] = useState(ResLis)
+    const [ListRec,setListRec] = useState([]);
 
-    return (
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData = async () => {
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.6888636&lng=86.9660638&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        
+        const json = await data.json()
+        setListRec(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+    } 
+
+    return ListRec.length == 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={()=>{
                     const filterlist = ListRec.filter(
-                        (res) => res.info.avgRating > 4.3
+                        (res) => res.info.avgRating > 4.1
                     )
                     setListRec(filterlist)
                 }}>

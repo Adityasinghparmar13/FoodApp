9ab1a033f76ca63757b61356afac0a51dcaf2d19
test1 <138242529+Adityasinghparmar13@users.noreply.github.com>
2025-01-27 14:38:26 +0530

    cors plugin

--- src/components/Body.js
+++ src/components/Body.js
@@ -12,7 +12,7 @@ const Body = () => {
     },[]);
 
     const fetchData = async () => {
-        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.6888636&lng=86.9660638&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const data = await fetch("https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.6888636&lng=86.9660638&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         
         const json = await data.json()
         setListRec(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
